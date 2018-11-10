963d4e88c57486ec216ab388f93c6526527f0749
Diana Lin <dlin@bcgsc.ca>
2018-11-09 16:53:20 -0800

    Fixed a naming bug in assessGaps

--- assessGaps.sh
+++ assessGaps.sh
@@ -9,15 +9,15 @@ filename=$(basename $1)
 extension="${filename##*.}"
 filename=$(basename $1 $extension)
 
-seqtk seq $1 > ${filename}.seqtk.fa
+seqtk seq $1 > ${filename}seqtk.fa
 if [ "$#" -eq 2 ]
 then
-	findGaps.py ${filename}.seqtk.fa $2
+	findGaps.py ${filename}seqtk.fa $2
 else
-	findGaps.py ${filename}.seqtk.fa
+	findGaps.py ${filename}seqtk.fa
 fi
 
 if [ -e "${filename}.seqtk.gaps.tsv" ]
 then
-	column -t -s$'\t' ${filename}.seqtk.gaps.tsv
+	column -t -s$'\t' ${filename}seqtk.gaps.tsv
 fi
