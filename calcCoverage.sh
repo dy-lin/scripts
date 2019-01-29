98cbcd376542d17b24e87779ca56949aa36a8ce4
Diana Lin <dlin@bcgsc.ca>
2019-01-29 14:20:40 -0800

    Modified calcCoverage to take interleaved reads; added splitFASTQ to split interleaved reads

--- calcCoverage.sh
+++ calcCoverage.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
+if [ "$#" -lt 1 ]
+then
+	echo "USAGE: $(basename $0) <FASTA file> <reads> <reads>" 1>&2
+	exit 1
+fi
 alignment=$1
 
 # Remove the file extension
@@ -19,6 +24,8 @@ then
 	result=$(median.R <(awk '{print $3}' <(samtools depth -a ${filename}.sorted.reads.bam)) 2> /dev/null)
 	median=$(echo $result | awk '{print $2}')
 	echo "Coverage: ${median}x"
+	rm ${filename}.sorted.bam
+
 # If there are three arguments, assume they are assembly and reads
 elif [ "$#" -eq 3 ]
 then
@@ -35,7 +42,7 @@ then
 		exit 1
 	fi
 
-	if [[ "$readsf" != *R?.f?.gz || "$readsb" != *R?.f?.gz ]]
+	if [[ "$readsf" != *.f?.gz || "$readsb" != *.f?.gz ]]
 	then
 		echo "Three arguments detected. Invalid reads files." 1>&2
 		echo "USAGE: $(basename $0) <FASTA file> <reads> <reads>" 1>&2
@@ -47,6 +54,47 @@ then
 	result=$(median.R <(awk '{print $3}' <(samtools depth -a ${filename}.reads.sorted.bam)) 2> /dev/null)
 	median=$(echo $result | awk '{print $2}')
 	echo "Coverage: ${median}x"
+	rm ${filename}.reads.bam
+	rm ${filename}.reads.sorted.bam
+	rm ${assembly}.sa
+	rm ${assembly}.amb
+	rm ${assembly}.ann
+	rm ${assembly}.bwt
+	rm ${assembly}.pac
+
+# If two, assume first is assembly second is interleaved reads.
+elif [ "$#" -eq 2 ]
+then
+	assembly=$1
+	reads=$2
+	temp=$(basename $assembly)
+	filename=${temp%.*}
+	if [[ "$assembly" != *.fa && "$assembly" != *.fasta && "$assembly" != *.f?.gz ]]
+	then
+		echo "Two arguments detected. Invalid assembly file." 1>&2
+		echo "USAGE: $(basename $0) <FASTA file> <inter-leaved reads>" 1>&2
+		exit 1
+	fi
+	if [[ "$reads" != *.f?.gz ]]
+	then
+		echo "Two arguments detected. Invalid reads files." 1>&2
+		echo "USAGE: $(basename $0) <FASTA file> <inter-leaved reads>" 1>&2
+		exit 1
+	fi
+	bwa index $assembly
+	bwa mem -t 48 $assembly $reads | samtools view -F 4 -b -o ${filename}.reads.bam
+	samtools sort ${filename}.reads.bam > ${filename}.reads.sorted.bam
+	result=$(median.R <(awk '{print $3}' <(samtools depth -a ${filename}.reads.sorted.bam)) 2> /dev/null)
+	median=$(echo $result | awk '{print $2}')
+	echo "Coverage: ${median}x"
+	rm ${filename}.reads.bam
+	rm ${filename}.reads.sorted.bam
+	rm ${assembly}.sa
+	rm ${assembly}.amb
+	rm ${assembly}.ann
+	rm ${assembly}.bwt
+	rm ${assembly}.pac
+
 # Or the arguments are invalid
 else
 	echo "USAGE: $(basename $0) <BAM file (preferably sorted)> OR" 1>&2
---
+++ splitFASTQ.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PROGRAM=$(basename $0)
+if [[ "$#" -ne 1 ]]
+then
+	echo "USAGE: $PROGRAM <GZIPPED FASTQ file>" 1>&2
+	echo "DESCRIPTION: Takes a gzipped FASTQ file as input, and separates the interleaved reads into two separate files." 1>&2
+	exit 1
+fi
+
+fastq=$1
+filename=$(basename $fastq ".fq.gz")
+set -eu -o pipefail
+
+seqtk seq -1 $fastq | pigz -p 64 > ${filename}.R1.fq.gz
+seqtk seq -2 $fastq | pigz -p 64 > ${filename}.R2.fq.gz
+
