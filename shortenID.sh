145e4e0be22353e8345bce3de84d226594b3f7d5
Diana Lin <dlin@bcgsc.ca>
2019-02-14 11:24:47 -0800

    Shortens sequence IDs into the first word for use with ORFfinder.

---
+++ shortenID.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PROGRAM=$(basename $0)
+if [[ "$#" -lt 1 ]]
+then
+	echo "USAGE: $PROGRAM <FASTA file(s)>" 1>&2
+	echo "DESCRIPTION: Takes FASTA file(s) and shortens the IDs to the first word." 1>&2
+	exit 1
+fi
+
+for fasta in "$@"
+do
+	filename=$(basename $fasta ".fa")
+	filename=$(basename $filename ".fasta")
+	while read id
+	do
+		echo $id | awk '{print $1}' >> ${filename}.shortened.fa
+		read seq
+		echo $seq >> ${filename}.shortened.fa
+	done < $fasta
+done
