cc120d50899857fe2807a4ca277cddbc5d5ea7cf
Diana Lin <dlin@bcgsc.ca>
2019-03-06 16:32:07 -0800

    run-ORFfinder now checks for existing files

--- run-ORFfinder.sh
+++ run-ORFfinder.sh
@@ -3,26 +3,43 @@ PROGRAM=$(basename $0)
 
 if [[ "$#" -eq 0 ]]
 then
-	echo "USAGE: $PROGRAM <FASTA file>" 1>&2
-	echo "DESCRIPTION: Takes a FASTA file and outputs both ORFs and CDS." 1>&2
+	echo "USAGE: $PROGRAM <FASTA file(s)>" 1>&2
+	echo "DESCRIPTION: Takes FASTA file(s) and outputs both ORFs and CDS." 1>&2
 	exit 1 
 fi
 
 mkdir -p log
 for fasta in $@
 do
-	if [[ "$fasta" == *.gz ]]
+	if [[ "$(readlink -f $fasta)" == *.gz ]]
 	then
 		name=$(basename $fasta ".fa.gz")
-		ORFfinder -in <(zcat $fasta) -outfmt 0 | seqtk seq - > ${name}.ORFs.faa 2>> log/${name}.log
-		ORFfinder -in <(zcat $fasta) -outfmt 1 | seqtk seq - > ${name}.cds.fa 2>> log/${name}.log
+
+		if [[ ! -e "${name}.ORFs.faa" ]]
+		then
+			echo "Running ORFfinder for gzipped $(basename $fasta), outputting an ORF FASTA file..." 1>&2
+			ORFfinder -in <(zcat $fasta) -outfmt 0 2>> log/${name}.log | seqtk seq - > ${name}.ORFs.faa
+		fi
+		if [[ ! -e "${name}.cds.fa" ]]
+		then
+			echo "Running ORFfinder for gzipped $(basename $fasta), outputting a CDS FASTA file..." 1>&2
+			ORFfinder -in <(zcat $fasta) -outfmt 1 2>> log/${name}.log | seqtk seq - > ${name}.cds.fa
+		fi
 	else
 		name=$(basename $fasta ".fa")
-		ORFfinder -in $fasta -outfmt 0 | seqtk seq - > ${name}.ORFs.faa 2>> log/${name}.log
-		ORFfinder -in $fasta -outfmt 1 | seqtk seq - > ${name}.cds.fa 2>> log/${name}.log
+		if [[ ! -e "${name}.ORFs.faa" ]]
+		then
+			echo "Running ORFfinder for $(basename $fasta), outputting an ORF FASTA file..." 1>&2
+			ORFfinder -in $fasta -outfmt 0 2>> log/${name}.log | seqtk seq - > ${name}.ORFs.faa
+		fi
+		if [[ ! -e "${name}.cds.fa" ]]
+		then
+			echo "Running ORFfinder for $(basename $fasta), outputting a CDS FASTA file..." 1>&2
+			ORFfinder -in $fasta -outfmt 1 2>> log/${name}.log | seqtk seq - > ${name}.cds.fa
+		fi
 	fi
 done
 
 
 
-
+echo "...Done." 1>&2
