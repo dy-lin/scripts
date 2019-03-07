3a4e9089104b581b85e49629183a538162d91559
Diana Lin <dlin@bcgsc.ca>
2019-03-06 16:33:40 -0800

    Copies final files (not symlinks) from scratch to backed up space.

---
+++ scratch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PROGRAM=$(basename $0)
+
+if [[ "$#" -eq 0 ]]
+then
+	echo "USAGE: $PROGRAM <Files/symlinks to copy>" 1>&2
+	echo "DESCRIPTION: Takes files/symlinks and copies to them to the current directory."
+	exit 1
+fi
+
+for file in $@
+do
+	if [ -L "$file" ]
+	then
+		cp "$(readlink -f $file)" $(basename $file)
+	else
+		cp $file .
+	fi
+done
+
+# Symlink those original files to this new directory?
