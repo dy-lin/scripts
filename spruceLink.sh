fd6c36e4df872e2b7a7f9f7c8b41348ff3feaa0d
Diana Lin <dlin@bcgsc.ca>
2019-03-19 14:00:42 -0700

    Updated symlinks

--- spruceLink.sh
+++ spruceLink.sh
@@ -16,25 +16,25 @@ fi
 
 if [[ "$spruce" == "WS77111" ]]
 then
-	ln -s /projects/spruceup_scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-labeled-nonSubsumed-9April2018.fa ${dir}/literature-AMPs.faa
-	ln -s /projects/spruceup_scratch/pglauca/WS77111/annotation/genome-annotation/Maker/GeneAndModelsPredFirstStepMaker/FourthStepCumulat/OutputMaker/LoweAED/WS77111_ntEdit_selShuf.all.polishedGeneseAED.gff ${dir}/genes.gff
-	ln -s /projects/spruceup_scratch/pglauca/WS77111/annotation/genome-annotation/Maker/GeneAndModelsPredFirstStepMaker/FourthStepCumulatNonNtEdit/OutputMaker/LoweAED/WS77111.all.maker.proteinsLoweAEDpolished.fasta ${dir}/proteins.faa
-	ln -s /projects/spruceup/pglauca/WS77111/assemblies/releases/version2/WS77111v2_release/WS77111-v2_1000plus_LGs.fa ${dir}/scaffolds.fa
-	ln -s /projects/spruceup_scratch/pglauca/WS77111/annotation/genome-annotation/Maker/GeneAndModelsPredFirstStepMaker/FourthStepCumulatNonNtEdit/OutputMaker/LoweAED/WS77111.all.maker.transcriptsLoweAEDpolished.fasta ${dir}/transcripts.fa
+	ln -fs /projects/spruceup_scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-labeled-nonSubsumed-9April2018.fa ${dir}/literature-AMPs.faa
+	ln -fs /projects/spruceup_scratch/pglauca/WS77111/annotation/genome-annotation/Maker/GeneAndModelsPredFirstStepMaker/FourthStepCumulatNonNtEdit/OutputMaker/LoweAED/WS77111.all.polishedGenesLoweAEDpolished.gff ${dir}/genes.gff
+	ln -fs /projects/spruceup_scratch/pglauca/WS77111/annotation/genome-annotation/Maker/GeneAndModelsPredFirstStepMaker/FourthStepCumulatNonNtEdit/OutputMaker/LoweAED/WS77111.all.maker.proteinsExtrcatTransPolish.fasta ${dir}/proteins.faa
+	ln -fs /projects/spruceup_scratch/pglauca/WS77111/annotation/genome-annotation/Maker/GeneAndModelsPredFirstStepMaker/FourthStepCumulatNonNtEdit/OutputMaker/LoweAED/WS77111.all.maker.transcriptsExtrcatTransPolish.fasta ${dir}/transcripts.fa
+	ln -fs /projects/spruceup/pglauca/WS77111/assemblies/releases/version2/WS77111v2_release/WS77111-v2_1000plus_LGs.fa ${dir}/scaffolds.fa
 elif [[ "$spruce" == "PG29" ]]
 then
-	ln -s /projects/spruceup_scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-labeled-nonSubsumed-9April2018.fa ${dir}/literature-AMPs.faa
-	ln -s /projects/spruceup_scratch/interior_spruce/PG29/annotation/genome-annotation/PG29v5/Maker/ThirdIterationCumulatNonntEdit/OutputMaker/LoweAED/PG29v5.all.maker.proteins1LoweAEDnoStopPolished.fasta ${dir}/proteins.faa
-	ln -s /projects/spruceup/interior_spruce/PG29/assemblies/releases/PG29-v5/PG29-v5_1000plus.fa ${dir}/scaffolds.fa
-	ln -s /projects/spruceup_scratch/interior_spruce/PG29/annotation/genome-annotation/PG29v5/Maker/ThirdIterationCumulatNonntEdit/OutputMaker/LoweAED/PG29v5.all.maker.transcripts1LoweAED.fasta ${dir}/transcripts.fa
-	ln -s /projects/spruceup_scratch/interior_spruce/PG29/annotation/genome-annotation/PG29v5/Maker/ThirdIterationCumulatNonntEdit/OutputMaker/LoweAED/PG29.all.polished.genesLoweAED.gff ${dir}/genes.gff
+	ln -fs /projects/spruceup_scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-labeled-nonSubsumed-9April2018.fa ${dir}/literature-AMPs.faa
+	ln -fs /projects/spruceup_scratch/interior_spruce/PG29/annotation/genome-annotation/PG29v5/Maker/ThirdIterationCumulatNonntEdit/OutputMaker/LoweAED/PG29v5.all.maker.transcriptsExtrcatTrans_polish.fasta ${dir}/transcripts.fa
+	ln -fs /projects/spruceup/interior_spruce/PG29/assemblies/releases/PG29-v5/PG29-v5_1000plus.fa ${dir}/scaffolds.fa
+	ln -fs  /projects/spruceup_scratch/interior_spruce/PG29/annotation/genome-annotation/PG29v5/Maker/ThirdIterationCumulatNonntEdit/OutputMaker/LoweAED/PG29v5.all.maker.proteinsExtrcatTrans_polish.fasta ${dir}/proteins.faa
+	ln -fs /projects/spruceup_scratch/interior_spruce/PG29/annotation/genome-annotation/PG29v5/Maker/ThirdIterationCumulatNonntEdit/OutputMaker/LoweAED/PG29.all.polished.genesLoweAED_polish.gff  ${dir}/genes.gff
 elif [[ "$spruce" == "Q903" ]]
 then
-	ln -s /projects/spruceup_scratch/psitchensis/Q903/annotation/genome-annotation/Maker/ThirdStepCumulat/OutputMaker/LoweAED/Q903.all.maker.proteinsLoweAED.fasta ${dir}/proteins.faa
-	ln -s /projects/spruceup/psitchensis/Q903/assembly/releases/v0.1beta/Q903_v0.1beta_1000plus.fa ${dir}/scaffolds.fa
-	ln -s /projects/spruceup_scratch/psitchensis/Q903/annotation/genome-annotation/Maker/ThirdStepCumulat/OutputMaker/LoweAED/Q903.all.maker.transcriptsLoweAED.fasta ${dir}/transcripts.fa
-	ln -s /projects/spruceup_scratch/psitchensis/Q903/annotation/genome-annotation/Maker/ThirdStepCumulat/OutputMaker/LoweAED/Q903.all.polishedGenesLoweAED.gff ${dir}/genes.gff
-	ln -s /projects/spruceup_scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-labeled-nonSubsumed-9April2018.fa ${dir}/literature-AMPs.faa
+	ln -fs /projects/spruceup_scratch/psitchensis/Q903/annotation/genome-annotation/Maker/ThirdStepCumulat/OutputMaker/LoweAED/Q903.all.maker.proteinsLoweAED.fasta ${dir}/proteins.faa
+	ln -fs /projects/spruceup/psitchensis/Q903/assembly/releases/v0.1beta/Q903_v0.1beta_1000plus.fa ${dir}/scaffolds.fa
+	ln -fs /projects/spruceup_scratch/psitchensis/Q903/annotation/genome-annotation/Maker/ThirdStepCumulat/OutputMaker/LoweAED/Q903.all.maker.transcriptsLoweAED.fasta ${dir}/transcripts.fa
+	ln -fs /projects/spruceup_scratch/psitchensis/Q903/annotation/genome-annotation/Maker/ThirdStepCumulat/OutputMaker/LoweAED/Q903.all.polishedGenesLoweAED.gff ${dir}/genes.gff
+	ln -fs /projects/spruceup_scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-labeled-nonSubsumed-9April2018.fa ${dir}/literature-AMPs.faa
 else
 	echo "Invalid genotype." 1>&2
 fi
