9a6bca88f742a70c27b3dc0199d358be55c02c3b
Diana Lin <dlin@bcgsc.ca>
2019-03-22 11:17:38 -0700

    Added support for paths given and multiple paths given.

--- clean-jackhmmer.sh
+++ clean-jackhmmer.sh
@@ -1,17 +1,99 @@
-#!/bin/bash
-
-for i in guide-*
+#!/bin/bash 
+force=false 
+gethelp=false 
+PROGRAM=$(basename $0)
+while getopts :hf opt
 do
-	if [[ -e "$i" ]]
-	then
-		rm $i
-	fi
+	case $opt in
+		h) gethelp=true;;
+		f) force=true;;
+		\?) echo "ERROR: $PROGRAM: invalid option: $opt" 1>&2; exit 1;;
+	esac
 done
 
-for i in jackhmmer_bs*
-do
-	if [[ -e "$i" ]]
+shift $((OPTIND-1))
+if [[ "$gethelp" = true ]]
+then
+	echo "USAGE: $PROGRAM [directory]" 1>&2
+	echo "DESCRIPTION: Removes JACKHMMER and BLAST output files in the specified directory. If no directory is given, the current working directory is used."  1>&2
+	echo -e "OPTIONS:\n\t-h\tShow help menu\n\t-f\tForce (Default: asks for confirmation before removing any files)" 1>&2
+	exit 1
+fi
+if [[ "$#" -eq 0 ]]
+then
+	dir=$(pwd)
+	if [[ "$force" = false ]]
 	then
-		rm $i
+		while read -p "Cleaning current directory --  Are you sure? (Y/N): " answer
+		do
+			case $answer in
+				Y) break;;
+				y) break;;
+				Yes) break;;
+				yes) break;;
+				N) echo "Aborted." 1>&2; exit 1;;
+				n) echo "Aborted." 1>&2; exit 1;;
+				no) echo "Aborted." 1>&2; exit 1;;	
+				No) echo "Aborted." 1>&2; exit 1;;
+				\?) echo "Invalid answer." 1>&2;;
+			esac
+		done
 	fi
-done
+	for i in ${dir}/guide-*
+	do
+		if [[ -e "$i" ]]
+		then
+			rm $i
+		fi
+	done
+
+	for i in ${dir}/jackhmmer_bs*
+	do
+		if [[ -e "$i" ]]
+		then
+			rm $i
+		fi
+	done
+else
+	for path in $@
+	do
+		dir=$(echo $path | sed 's/\/$//')
+		if [[ ! -d "$dir" ]]
+		then
+			echo "$dir does not exist." 1>&2
+			continue
+		fi
+		if [[ "$force" = false ]]
+		then
+			while read -p "Cleaning ${dir} --  Are you sure? (Y/N): " answer
+			do
+				case $answer in
+					Y) break;;
+					y) break;;
+					Yes) break;;
+					yes) break;;
+					N) echo "Aborted." 1>&2; exit 1;;
+					n) echo "Aborted." 1>&2; exit 1;;
+					no) echo "Aborted." 1>&2; exit 1;;	
+					No) echo "Aborted." 1>&2; exit 1;;
+					\?) echo "Invalid answer." 1>&2;;
+				esac
+			done
+		fi
+		for i in ${dir}/guide-*
+		do
+			if [[ -e "$i" ]]
+			then
+				rm $i
+			fi
+		done
+
+		for i in ${dir}/jackhmmer_bs*
+		do
+			if [[ -e "$i" ]]
+			then
+				rm $i
+			fi
+		done
+	done
+fi
