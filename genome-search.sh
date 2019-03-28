0aa8e59c7c7d18c13ee22e97354dae0fcb4c69a8
Diana Lin <dlin@bcgsc.ca>
2019-03-28 15:53:55 -0700

    Added blastn

--- genome-search.sh
+++ genome-search.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
-
-if [[ "$(pwd)" != */annotation/amp ]]
+PROGRAM=$(basename $0)
+if [[ "$#" -ne 5 ]]
 then
-	echo "Wrong working directory." 1>&2
-	echo "Working directory must be: /projects/spruceup/scratch/spruce/genotype/annotation/amp" 1>&2
+	echo "USAGE: $PROGRAM <class of AMPs> <literature AMPs (prot) > <literature AMPs (nucl)> <genome assembly> <output prefix>"
 	exit 1
 fi
-
-amps=$1
-mRNA=$2
-scaffold=$3
-prefix=$4
+# if [[ "$(pwd)" != /annotation/amp ]]
+# then
+#	echo "Wrong working directory." 1>&2
+#	echo "Working directory must be: /projects/spruceup/scratch/spruce/genotype/annotation/amp" 1>&2
+#	exit 1
+# fi
+class=$1
+if [[ "$class" != *s ]]
+then
+	class="${class}s"
+fi
+amps=$2
+mRNA=$3
+scaffold=$4
+prefix=$5
 
 # Make tblastn-genome directory
-mkdir -p tblastn-genome
-cd tblastn-genome
-blast.sh nucl $amps $scaffold
-cd ..
+mkdir -p tblastn-genome/${class}
+cd tblastn-genome/${class}
+blast.sh tblastn $amps $scaffold
+blast.sh blastn $mRNA $scaffold
+cd ../..
+
+
 # Make gmap-genome directory
-mkdir -p gmap-genome
-run-gmap.sh GMAP-index 48 $prefix $mRNA
+mkdir -p gmap-genome/${class}
+cd gmap-genome/${class}
+run-gmapl.sh ../GMAP-index 48 $prefix $mRNA
+cd ../..
+
 
