34e45b7609895119a7824fefea1df1d8b0e713b2
Diana Lin <dlin@bcgsc.ca>
2019-03-28 15:53:24 -0700

    Added thread option and other blast programs.

--- blast.sh
+++ blast.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
 PROGRAM=$(basename $0)
+threads=48
+gethelp=false
+while getopts :ht $opt
+do
+	case $opt in
+		h) gethelp=true;;
+		t) threads=$OPTARG;;
+		\?) echo "ERROR: $PROGRAM: Invalid option $OPTARG" 1>&2; exit 1;;
+	esac
+done
+shift $((OPTIND-1))
 
-if [[ "$#" -ne 3 ]]
+if [[ "$#" -ne 3 && "$#" -ne 4 ]] || [[ "$gethelp" = true ]]
 then
-	echo "USAGE: $PROGRAM <nucl or prot> <query> <subject>" 1>&2
+	echo "USAGE: $PROGRAM <BLAST program> <query> <subject> [output directory]" 1>&2
 	echo "DESCRIPTION: Blasts the query file against the subject file." 1>&2
+	echo -e "OPTIONS:\n\t-h\tShow help menu\n\t-t\tnumber of threads" 1>&2
 	exit 1
 fi
+blast=$1
 
-dbtype=$1
+dbtype=$stype
 query=$2
 db=$3
-qname=${query%.*}
-sname=${db%.*}
-## Make sure that the subject sequences given are actually the type they claim to be
-sdetected=$(findType.sh $db)
-if [[ "$sdetected" == "$dbtype" ]]
+qname=$(basename ${query%.*})
+sname=$(basename ${db%.*})
+if [[ -z "$4" ]]
 then
-	makeblastdb -in $db -out $sname -dbtype $dbtype
+	dir=$(pwd)
 else
-	echo "Detected subject file type is: $sdetected, but given $dbtype." 1>&2
-	exit 1
+	dir=$4
 fi
 
-qdetected=$(findType.sh $query)
+# qdetected=$(findType.sh $query)
 
-echo "Query file type detected: $qdetected" 1>&2
-echo "Subject file type detected: $sdetected" 1>&2
-if [[ "$qdetected" == "nucl" && "$dbtype" == "nucl" ]]
-then
-	echo "Running blastn..." 1>&2
-	blastn -query $query -db $sname -out ${qname}.aln.blastn -num_threads 48
-	blastn -query $query -db $sname -out ${qname}.tsv.blastn -num_threads 48 -outfmt '6 std qcovs'
-fi
+# if [[ "$qdetected" != "$qtype" ]]
+# then
+#	echo "Detected query file type is: $qdetected, but given $qtype." 1>&2
+#	read -p "Continue with $dbtype? (Y/N): " answer
+#	if [[ "$answer" =! [Nn] ]] 
+#	then
+#		exit 1
+#	fi
+#	qdetected=$qtype
+# fi
 
-if [[ "$qdetected" == "prot" && "$dbtype" == "nucl" ]]
-then
-	echo "Running tblastn..." 1>&2
-	tblastn -query $query -db $sname -out ${qname}.aln.tblastn -num_threads 48
-	tblastn -query $query -db $sname -out ${qname}.tsv.tblastn -num_threads 48 -outfmt '6 std qcovs'
-fi
+case $blast in
+	tblastn) qtype=prot; stype=nucl;dbtype=$stype;if [[ "$(ls ${dir}/${name}*.nhr 2> /dev/null | wc -l)" -eq 0 ]];then makeblastdb -in $db -out ${dir}/$sname -dbtype $dbtype; fi;echo "Running tblastn..." 1>&2;tblastn -query $query -db $dir/$sname -out $dir/${qname}.aln.tblastn -num_threads $threads;tblastn -query $query -db $dir/$sname -out $dir/${qname}.tsv.tblastn -num_threads $threads -outfmt '6 std qcovs';;
+blastn) qtype=nucl; stype=nucl;dbtype=$stype;if [[ "$(ls ${dir}/${name}*.nhr 2> /dev/null | wc -l)" -eq 0 ]];then makeblastdb -in $db -out ${dir}/$sname -dbtype $dbtype; fi;echo "Running blastn..." 1>&2;blastn -query $query -db $dir/$sname -out $dir/${qname}.aln.blastn -num_threads $threads;blastn -query $query -db $dir/$sname -out $dir/${qname}.tsv.blastn -num_threads $threads -outfmt '6 std qcovs';;
+	blastp) qtype=prot; stype=prot;dbtype=$stype;if [[ "$(ls ${dir}/${name}*.phr 2> /dev/null | wc -l)" -eq 0 ]];then makeblastdb -in $db -out ${dir}/$sname -dbtype $dbtype; fi;echo "Running blastp..." 1>&2;blastp -query $query -db $dir/$sname -out $dir/${qname}.aln.tblastn -num_threads $threads;blastp -query $query -db $dir/$sname -out $dir/${qname}.tsv.tblastn -num_threads $threads -outfmt '6 std qcovs';;
+blastx) qtype=nucl; stype=prot;dbtype=$stype;if [[ "$(ls ${dir}/${name}*.phr 2> /dev/null | wc -l)" -eq 0 ]];then makeblastdb -in $db -out ${dir}/$sname -dbtype $dbtype; fi;echo "Running blastx..." 1>&2;blastx -query $query -db $dir/$sname -out $dir/${qname}.aln.blastx -num_threads $threads;blastx -query $query -db $dir/$sname -out $dir/${qname}.tsv.blastx -num_threads $threads -outfmt '6 std qcovs';;
+	tblastx) qtype=prot; stype=prot;dbtype=$stype;if [[ "$(ls ${dir}/${name}*.phr 2> /dev/null | wc -l)" -eq 0 ]];then makeblastdb -in $db -out ${dir}/$sname -dbtype $dbtype; fi;echo "Running tblastx..." 1>&2;tblastx -query $query -db $dir/$sname -out $dir/${qname}.aln.tblastx -num_threads $threads;tblastx -query $query -db $dir/$sname -out $dir/${qname}.tsv.tblastx -num_threads $threads -outfmt '6 std qcovs';;
+	\?) echo "ERROR: Unrecognized BLAST program." 1>&2 ; exit 1;;
+esac
 
-if [[ "$qdetected" == "prot" && "$dbtype" == "prot" ]]
-then
-	echo "Running blastp..." 1>&2
-	blastp -query $query -db $sname -out ${qname}.aln.tblastn -num_threads 48
-	blastp -query $query -db $sname -out ${qname}.tsv.tblastn -num_threads 48 -outfmt '6 std qcovs'
-fi
 
-if [[ "$qdetected" == "nucl" && "$dbtype" == "prot" ]]
-then
-	echo "Running blastx..." 1>&2
-	blastx -query $query -db $sname -out ${qname}.aln.blastx -num_threads 48
-	blastx -query $query -db $sname -out ${qname}.tsv.blastx -num_threads 48 -outfmt '6 std qcovs'
-fi
