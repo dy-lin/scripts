bab525c2d07d1e65480e37a9e4991efb8148a79c
Diana Lin <dlin@bcgsc.ca>
2019-04-17 15:39:35 -0700

    Added support for pipe input

--- splitFASTA.sh
+++ splitFASTA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+stdin=$(cat)
 PROGRAM=$(basename $0)
-
-if [[ "$#" -lt 1 ]]
+if [[ ! -p "/dev/stdin" && "$#" -lt 1 ]]
 then
 	echo "USAGE: $PROGRAM <FASTA file(s)>" 1>&2
 	echo "DESCRIPTION: Takes FASTA file(s) as input, and outputs each sequence in a separate FASTA file." 1>&2
@@ -9,14 +9,31 @@ then
 fi
 
 # Read ID and sequence
-while read id
-do
-	filename=$(echo $id | awk '{print $1}' | sed 's/^>//').fa
-	read seq
-	# If output file already exists, there are identical sequences in the file(s) - only write one.
-	if [[ ! -e "$filename" ]]
-	then
-		echo $id >> $filename
-		echo $seq >> $filename
-	fi
-done < <(cat $*)
+if [[ "$#" -ge 1 ]]
+then
+	while read id
+	do
+		filename=$(echo $id | awk '{print $1}' | sed 's/^>//').fa
+		read seq
+		# If output file already exists, there are identical sequences in the file(s) - only write one.
+		if [[ ! -e "$filename" ]]
+		then
+			echo $id >> $filename
+			echo $seq >> $filename
+		fi
+	done < <(cat $*)
+elif [[ -p "/dev/stdin" ]]
+then
+	while read id
+	do
+		filename=$(echo $id | awk '{print $1}' | sed 's/^>//').fa
+		read seq
+		# If output file already exists, there are identical sequences in the file(s) - only write one.
+		if [[ ! -e "$filename" ]]
+		then
+			echo $id >> $filename
+			echo $seq >> $filename
+		fi
+	done < <(echo "$stdin")
+fi
+
