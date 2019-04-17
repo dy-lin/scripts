ceb3a3e5b0df0f481c25269b340018a02d8ce592
Diana Lin <dlin@bcgsc.ca>
2019-04-17 15:41:57 -0700

    Renaming genes and transcripts in the GFF after NCBI submission of spruce genomes.

---
+++ renameGFF.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ "$#" -eq 0 ]]
+then
+	echo "USAGE: $(basename $0) <MAP> <GFF>" 1>&2
+	exit 1
+fi
+
+map=$1
+shift
+
+for gff in $@
+do
+	genes=$(awk -F "\t" '/\tgene\t/ {print $9}' $gff | awk -F ";" '{print $1}' | sed 's/ID=//')
+	mrnas=$(awk -F "\t" '/\tmRNA\t/ {print $9}' $gff | awk -F ";" '{print $1}' | sed 's/ID=//')
+	for mrna in $mrnas
+	do
+		sub=$(awk -v var="$mrna" '{if($1==var) print $2}' $map)
+		if [[ ! -z "$sub" ]]
+		then
+			sed -i "s/$mrna/$sub/g" $gff
+		fi
+	done
+	for gene in $genes
+	do
+		sub=$(awk -v var="$gene" '{if($1==var) print $2}' $map)
+		if [[ ! -z "$sub" ]]
+		then
+			sed -i "s/$gene/$sub/g" $gff
+		fi
+	done
+done
