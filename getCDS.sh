582a90e6cda7919c9322e044efc01576ad4fe1d0
Diana Lin <dlin@bcgsc.ca>
2019-08-02 14:53:13 -0700

    Added -join in gt extractfeat, removed manual joining

--- getCDS.sh
+++ getCDS.sh
@@ -10,38 +10,40 @@ gff=$2
 name=$(basename "${gff%%.*}")
 # Fetches the CDS separately for each GFF entry
 
-if [[ -e "${name}.temp.fa" ]]
-then
-	rm "${name}.temp.fa"
-fi
+# if [[ -e "${name}.temp.fa" ]]
+# then
+# 	rm "${name}.temp.fa"
+# fi
 
-gt extractfeat -retainids -matchdescstart -type CDS -seqfile $scaf -o ${name}.temp.fa $gff 2> /dev/null
+gt extractfeat -join -retainids -matchdescstart -type CDS -seqfile $scaf -o ${name}.cds.fa $gff 2> /dev/null
 if [[ "$?" -eq 1 ]]
 then
 	gt gff3 -sort -retainids $gff > ${name}.sorted.gff
 	if [[ "$?" -eq 0 ]]
 	then
-		gt extractfeat -retainids -matchdescstart -type CDS -seqfile $scaf -force -o ${name}.temp.fa ${name}.sorted.gff
+		gt extractfeat -join -retainids -matchdescstart -type CDS -seqfile $scaf -force -o ${name}.cds.fa ${name}.sorted.gff
 	else
 		exit 1
 	fi
 fi
 
+sed -i 's/(joined)$//' ${name}.cds.fa
 # Get unique FASTA headers
 # For each one, seqtk subseq, then combine together to write a new one
 
 # If ${name}.cds.fa already exists, delete
-if [[ -e "${name}.cds.fa" ]]
-then
-	rm "${name}.cds.fa"
-fi
+# if [[ -e "${name}.cds.fa" ]]
+# then
+# 	rm "${name}.cds.fa"
+# fi
 
-for header in $(grep '^>' ${name}.temp.fa | sort -u )
-do
-	echo "$header" >> ${name}.cds.fa
-#	seqtk subseq ${name}.temp.fa <(echo "$header") | grep -v '^>' | tr -d '\n' >> ${name}.cds.fa
-	grep -A1 "$header" ${name}.temp.fa | grep -v '^>' | tr -d '\n' >> ${name}.cds.fa
-	echo >> ${name}.cds.fa
-done
-
-rm ${name}.temp.fa
+# cat ${name}.temp.fa
+# for header in $(grep '^>' ${name}.temp.fa | sort -u )
+# do
+# 	echo "$header" >> ${name}.cds.fa
+# #	seqtk subseq ${name}.temp.fa <(echo "$header") | grep -v '^>' | tr -d '\n' >> ${name}.cds.fa
+# 	grep -A1 "$header" ${name}.temp.fa | grep -v '^>' | tr -d '\n' >> ${name}.cds.fa
+# 	echo >> ${name}.cds.fa
+# done
+# 
+# rm ${name}.temp.fa
