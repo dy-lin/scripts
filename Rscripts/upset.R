895849427276eff74bafa559f5c565b645bc44fa
Diana Lin <dlin@bcgsc.ca>
2019-08-20 16:31:43 -0700

    Script for making upset plots

---
+++ Rscripts/upset.R
@@ -0,0 +1,82 @@
+#!/usr/bin/env Rscript
+
+# summarize transcript TPMs to genes
+
+library(dplyr)
+library(UpSetR)
+
+defensins <- c("E0M31_00027086", "E0M31_00027087", "E0M31_00055415", "E0M31_00093276", "E0M31_00093277")
+
+args = commandArgs(trailingOnly=TRUE)
+infile <- "/projects/spruceup_scratch/psitchensis/Q903/annotation/amp/kallisto/control/annotated_transcripts/replicates/control.tsv"
+# infile <- args[1]
+# treatment <- args[2]
+treatment <- "control"
+
+df <- read.csv(infile, header = TRUE, sep="\t")
+
+grouped <- group_by(df,target_id)
+summarised <- summarise(grouped, sum1=sum(tpm), sum2=sum(tpm.1), sum3=sum(tpm.2), sum4=sum(tpm.3))
+
+metadata<-"/projects/spruceup/scratch/psitchensis/Q903/annotation/amp/kallisto/samples_wpw.csv"
+kallisto_dir<-"/projects/spruceup/scratch/psitchensis/Q903/annotation/amp/kallisto"
+specific_dir<-"annotated_transcripts/replicates"
+tx2gene_dict<-"/projects/spruceup_scratch/psitchensis/Q903/annotation/amp/kallisto/deseq/tx2gene.csv"
+outfile<-"test.png"
+
+# load METADATA file
+# At least two columns, 'sample' and 'treatment'
+samples <- read.csv(metadata, header = TRUE, sep=",")
+num_treatments=length(unique(samples$treatment))
+num_reps=nrow(samples)/num_treatments
+
+files <- file.path(kallisto_dir,samples$treatment, specific_dir, samples$sample, "abundance.h5")
+names(files) <- samples$sample
+tx2gene <- read.csv(tx2gene_dict)
+txi.kallisto <- tximport(files, type="kallisto", tx2gene=tx2gene)
+
+
+# load METADATA file
+# At least two columns, 'sample' and 'treatment'
+samples <- read.csv(metadata, header = TRUE, sep=",")
+
+
+files <- file.path(kallisto_dir,samples$treatment, specific_dir, samples$sample, "abundance.h5")
+names(files) <- samples$sample
+tx2gene <- read.csv(tx2gene_dict)
+txi.kallisto <- tximport(files, type="kallisto", tx2gene=tx2gene)
+df <- as.data.frame(txi.kallisto$abundance)
+
+if (num_reps > 1 && num_treatments == 2) {
+    df_avg <- df %>% transmute(avg1=rowMeans(df[,1:num_reps]),
+                               avg2=rowMeans(df[,(num_reps+1):(num_reps*2)]))
+}
+
+if (num_reps > 1 && num_treatments == 3) {
+    df_avg <- df %>% transmute(avg1=rowMeans(df[,1:num_reps]),
+                               avg2=rowMeans(df[,(num_reps+1):(num_reps*2)]), 
+                               avg3=rowMeans(df[,((num_reps*2)+1):(num_reps*3)]))
+}
+
+if (num_reps ==1 ) {
+    df_avg <- df
+}
+colnames(df_avg) <- unique(samples$treatment)
+df_avg$gene <- rownames(df)
+rownames(df_avg) <- rownames(df)
+df_avg <- df_avg[,c(4,1,2,3)]
+
+flipped <- as.data.frame(df_avg[defensins,] %>% t)[-1,]
+
+flipped_upset <- flipped
+
+for (i in 1:length(defensins)) {
+    flipped_upset[,i] <- as.numeric(as.character(flipped_upset[,i]))
+}
+
+flipped_upset[flipped_upset >=1] <- 1
+flipped_upset[flipped_upset < 1] <- 0
+
+
+upset(flipped_upset, nsets=3, text.scale=2)
+
