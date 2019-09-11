55330677cd5afcd9f6936dace4d3c52e4e5168d6
Diana Lin <dlin@bcgsc.ca>
2019-09-11 16:17:04 -0700

    Add file existence check

--- alignment/run-ORFfinder.sh
+++ alignment/run-ORFfinder.sh
@@ -11,32 +11,37 @@ fi
 mkdir -p log
 for fasta in $@
 do
-	if [[ "$(readlink -f $fasta)" == *.gz ]]
+	if [[ -e "$fasta" ]]
 	then
-		name=$(basename $fasta ".fa.gz")
+		if [[ "$(readlink -f $fasta)" == *.gz ]]
+		then
+			name=$(basename $fasta ".fa.gz")
 
-		if [[ ! -e "${name}.ORFs.faa" ]]
-		then
-			echo "Running ORFfinder for gzipped $(basename $fasta), outputting an ORF FASTA file..." 1>&2
-			ORFfinder -in <(zcat $fasta) -outfmt 0 2>> log/${name}.log | seqtk seq - > ${name}.ORFs.faa
-		fi
-		if [[ ! -e "${name}.cds.fa" ]]
-		then
-			echo "Running ORFfinder for gzipped $(basename $fasta), outputting a CDS FASTA file..." 1>&2
-			ORFfinder -in <(zcat $fasta) -outfmt 1 2>> log/${name}.log | seqtk seq - > ${name}.cds.fa
+			if [[ ! -e "${name}.ORFs.faa" ]]
+			then
+				echo "Running ORFfinder for gzipped $(basename $fasta), outputting an ORF FASTA file..." 1>&2
+				ORFfinder -in <(zcat $fasta) -outfmt 0 2>> log/${name}.log | seqtk seq - > ${name}.ORFs.faa
+			fi
+			if [[ ! -e "${name}.cds.fa" ]]
+			then
+				echo "Running ORFfinder for gzipped $(basename $fasta), outputting a CDS FASTA file..." 1>&2
+				ORFfinder -in <(zcat $fasta) -outfmt 1 2>> log/${name}.log | seqtk seq - > ${name}.cds.fa
+			fi
+		else
+			name=$(basename $fasta ".fa")
+			if [[ ! -e "${name}.ORFs.faa" ]]
+			then
+				echo "Running ORFfinder for $(basename $fasta), outputting an ORF FASTA file..." 1>&2
+				ORFfinder -in $fasta -outfmt 0 2>> log/${name}.log | seqtk seq - > ${name}.ORFs.faa
+			fi
+			if [[ ! -e "${name}.cds.fa" ]]
+			then
+				echo "Running ORFfinder for $(basename $fasta), outputting a CDS FASTA file..." 1>&2
+				ORFfinder -in $fasta -outfmt 1 2>> log/${name}.log | seqtk seq - > ${name}.cds.fa
+			fi
 		fi
 	else
-		name=$(basename $fasta ".fa")
-		if [[ ! -e "${name}.ORFs.faa" ]]
-		then
-			echo "Running ORFfinder for $(basename $fasta), outputting an ORF FASTA file..." 1>&2
-			ORFfinder -in $fasta -outfmt 0 2>> log/${name}.log | seqtk seq - > ${name}.ORFs.faa
-		fi
-		if [[ ! -e "${name}.cds.fa" ]]
-		then
-			echo "Running ORFfinder for $(basename $fasta), outputting a CDS FASTA file..." 1>&2
-			ORFfinder -in $fasta -outfmt 1 2>> log/${name}.log | seqtk seq - > ${name}.cds.fa
-		fi
+		echo "$(basename $fasta) does not exist." 1>&2
 	fi
 done
 
